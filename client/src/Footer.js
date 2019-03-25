42e10eccb5a4050334d77f125ba3fda1d6477ef7
anakellyc <anakcampos@gmail.com>
2019-03-25 01:57:16 +0100

    adding client

---
+++ client/src/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import FontAwesome from "react-fontawesome";
+
+export default () => (
+  <footer>
+    <a
+      href="https://medium.com/p/862d59583105"
+      title="LinkedIn"
+      className="small-button linkedin"
+    >
+      <FontAwesome name="medium" />
+    </a>
+    <a
+      href="https://github.com/anakellyc"
+      title="Github profile"
+      className="small-button github"
+    >
+      <FontAwesome name="github" />
+    </a>
+  </footer>
+);
